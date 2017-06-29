4819d167081d7ad073f78f18a8c67d45bd2468cc
sky <mingyuliao@foxmail.com>
2017-06-29 23:24:21 +0800

    add  my secretary page

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ var apiBaseUrl = "http://fcsyapi.liaoyuming.cn/api/"
 
 var config = {
   apiBaseUrl,
+  loginUrl: apiBaseUrl + 'login',
   questionnaireShowUrl: apiBaseUrl + 'questionnaire/show',
+  questionnaireIndexUrl: apiBaseUrl + 'questionnaire',
 };
 
 module.exports = config
