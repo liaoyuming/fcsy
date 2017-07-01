877f7a3d1d53d439e6cd73693137f9b47f526255
motecshine <shinemotec@gmail.com>
2017-07-02 03:40:20 +0800

    add change resume status

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ var config = {
   loginUrl: apiBaseUrl + 'login',
   registerUrl: apiBaseUrl + 'register',
   checkRegisterUrl: apiBaseUrl + 'register/check',
+  changeResumeStatus: apiBaseUrl + 'changeResumeStatus'
 };
 
 module.exports = config;
