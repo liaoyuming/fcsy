5b5deaff1317e8aa2b698ba22e7ee42ad18a753e
sky <mingyuliao@foxmail.com>
2017-07-06 12:48:45 +0800

    update proposal page

--- pages/proposal/proposal.js
+++ pages/proposal/proposal.js
@@ -5,27 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    character: {},
+    character: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getCharacter();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  getCharacter: function () {
     var that = this;
     wx.request({
       url: getApp().globalData.config.characterShowUrl,
@@ -37,7 +27,24 @@ Page({
           character: res.data.data,
         });
       }
-    })
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    if (!this.data.character) {
+      this.getCharacter();
+    }
+    console.log(!this.data.character);
   },
 
   /**
